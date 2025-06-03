c464a0f5b68a89fe6863f3016e9b93003dc9bf44
Cenk Kağan Çakır <69301124+Speellz@users.noreply.github.com>
2025-06-03 21:22:23 +0300

    Rename application class to JobRecruitmentApplication

--- src/main/java/com/jobrecruitment/JobRecruitmentApplication.java
+++ src/main/java/com/jobrecruitment/JobRecruitmentApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobrecruitmentApplication {
+public class JobRecruitmentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JobrecruitmentApplication.class, args);
+        SpringApplication.run(JobRecruitmentApplication.class, args);
     }
 }
